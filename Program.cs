039ad73c64cb5b0c73224b98b203e0bba74bdcca
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2023-12-03 02:59:21 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GA_LinearSearch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

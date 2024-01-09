20882fcb8735ab185c8175420684c67f6343bbc1
winterednalynn <ednalynnnl@gmail.com>
2024-01-08 20:50:16 -0800

    COMPLETE 1.8.24

---
+++ Movies.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GA_LinearSearch
+{
+    public class Movies
+    {
+        public Movies(string name, string category, int rate) // CONSTRUCTOR FOR MOVIES 
+        {
+            Name = name;
+            Category = category;
+            this.rate = rate;
+        }
+        // PROPERTIES 
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public int rate { get; set; }
+
+        public override string ToString() // WHEN .TOSTRING IS CALLED, THIS FORMAT WILL PROMPT. 
+        {
+            return $"{Name} - {Category} - Rate {rate}"; 
+        }
+    }
+   
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,196 @@
 ﻿using static System.Formats.Asn1.AsnWriter;
 
 namespace GA_LinearSearch
-{
+{ //EDNA LYNN LAXA 
+  //COMPUTER PROGRAMMING V 
+  //ASSIGNMENT: GUIDED ASSIGNMENT 3 : LINEAR SEARCH 
+  //JANUARY 7, 2023 
     internal class Program
     {
-
         // Declare the global arrays
         static string[] storeDirectory;
         static string[] storeCategories;
         static Store[] stores;
 
+        //Global Declared array - MINES 
+        static string[] movieCategory;
+        static string[] movieName;
+        static int[] movieRate;
+        static Movies[] theMovie;  
+
         static void Main(string[] args)
+        {
+            ednalynnPL();
+
+            //SEARCH BY MOVIE 
+            Console.WriteLine("Movie Search");
+            Console.WriteLine("Walk to Remember: ");
+            Console.WriteLine(SearchMovie(movieName, "Walk to remember").ToString()); //TRUE 
+            Console.WriteLine();
+            Console.WriteLine("Evil Dead Rising");
+            Console.WriteLine(SearchMovie(movieName, "Evil Dead Rising").ToString()) ; // TRUE 
+            Console.WriteLine();
+            Console.WriteLine("Emoji");
+            Console.WriteLine(SearchMovie(movieName, "Emoji").ToString()); // FALSE 
+
+            //SEARCH BY CATEGORIES 
+            Console.WriteLine();
+            Console.WriteLine("Movie Categories");
+            Console.WriteLine("Comedy");
+            Console.WriteLine(MovieByCategory(movieCategory,"Comedy").ToString()); //TRUE
+            Console.WriteLine();
+            Console.WriteLine("Based on a true story");
+            Console.WriteLine(MovieByCategory(movieCategory,"Based on a true story").ToString()); // False
+            
+           
+            //SEARCH BY RATING 
+            Console.WriteLine();
+            Console.WriteLine("A movie with 5 star Rating ");
+            Console.WriteLine(FindFirstMovieByRating(5, theMovie).ToString());  
+            Console.WriteLine();
+            Console.WriteLine("Movie with a 3 star rating ");
+            Console.WriteLine(FindFirstMovieByRating(3, theMovie).ToString());
+
+            //INDEX PLACEMENT 
+            Console.WriteLine();
+            Console.WriteLine("Index Placement In Movie List ");
+            Console.WriteLine();
+            Console.WriteLine("Napoleon Dynamite is located in: ");
+            Console.WriteLine(AllMoviePlacement(movieName,"Napoleon Dynamite"));
+            Console.WriteLine();
+            Console.WriteLine("Walk to remember is located in: ");
+            Console.WriteLine(AllMoviePlacement(movieName, "Walk to remember"));
+
+
+
+
+
+
+            //Console.WriteLine("Contains");
+            //Console.WriteLine(ContainsStore(storeDirectory, "Aqua Adventures").ToString()); // True
+            //Console.WriteLine(ContainsStore(storeDirectory, "Box Lunch").ToString()); // False
+
+            //Console.WriteLine("Store By Index");
+            //Console.WriteLine(StoreByCategory(storeCategories, "Entertainment").ToString()); // 1
+            //Console.WriteLine(StoreByCategory(storeCategories, "Food").ToString()); // -1
+
+            //Console.WriteLine("All stores of a category");
+            //Console.WriteLine(AllStoresOfACategory(storeCategories, "Café & Bakery").Count); // 2
+            //Console.WriteLine(AllStoresOfACategory(storeCategories, "Food").Count); // 0
+
+            //Console.WriteLine("All stores on a floor");
+
+            //List<Store> storeResults = AllStoresOnLevel(stores, "A");
+            //Console.WriteLine(storeResults.Count); // 4
+            //foreach (Store store in storeResults) { Console.WriteLine($"{store.Name} - {store.StoreLocation}"); }
+            //Console.WriteLine(AllStoresOnLevel(stores, "E").Count); // 0
+
+
+        }
+
+        static void ednalynnPL() // MY PRELOAD 
+        {
+            movieCategory = new string[]
+           {
+            "Comedy",
+            "Thriller",
+            "Horror",
+            "Crime",
+            "Mystery",
+            "Action",
+            "Science Fiction",
+            "Romance",
+            "Animation",
+            "Musical"
+           };
+
+            movieName = new string[]
+            {
+                "Napoleon Dynamite",
+                "Gone Girl",
+                "Evil Dead Rising",
+                "Sound of Freedom",
+                "A simple favor",
+                "Plane",
+                "Simone",
+                "Walk to remember",
+                "Spirited away",
+                "Rent"
+
+
+            };
+
+            movieRate = new int[]
+            {
+                5,
+                4,
+                3,
+                2,
+                1
+            };
+
+            theMovie = new Movies[]
+            {
+
+                new Movies("Napoleon Dynamite", "Comedy", 5 ),
+                new Movies("Gone Girl", "Thriller", 4),
+                new Movies("Evil Dead Rising", "Horror", 5),
+                new Movies("Sound Of Freedom", "Crime", 5),
+                new Movies("A simple favor", "Mystery", 3),
+                new Movies("Plane", "Action", 4),
+                new Movies("Simone", "Science Fiction", 2),
+                new Movies("Walk to Remember", "Romance", 5),
+                new Movies("Spirited away", "Animation", 5),
+                new Movies("Rent", "Musical", 5)
+            }; 
+        }
+        public static bool SearchMovie(string[] movieList, string searchCommand) // SEARCH METHOD FOR MOVIE 
+        {
+            foreach (string movie in movieList) // USING FOR EACH TO LOOP THORUGH MOVIE LIST 
+            {
+                if (movie == searchCommand)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool MovieByCategory(string[] movieList, string searchCommand) // SEARCH THROUGH CATEGORIES 
+        {
+            foreach(string movie in movieList)
+            {
+                if(movie == searchCommand)
+                {
+                    return true; 
+                }
+            }
+            return false; 
+        }
+        public static List<int> AllMoviePlacement(string[] movieList, string searchCommand) //INDEX FINDER 
+        {
+            List<int> index = new List<int>();
+            for (int i = 0; i < movieList.Length; i++)
+            {
+                if (movieList[i] == searchCommand)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            return index;
+           
+        }
+
+        public static Movies FindFirstMovieByRating(int searchRating, IEnumerable<Movies> movies) // LOCATE A MOVIE OF REQUESTED RATE
+        {
+            return movies.FirstOrDefault(movie => movie.rate == searchRating);
+        }
+
+        
+
+        //**************************SAMPLE CODE***********************************
+
+
+        public static void ProfessorsExample()
         {
             Preload();
 
@@ -29,11 +209,13 @@ namespace GA_LinearSearch
             Console.WriteLine("All stores on a floor");
             List<Store> storeResults = AllStoresOnLevel(stores, "A");
             Console.WriteLine(storeResults.Count); // 4
-            foreach(Store store in storeResults) { Console.WriteLine($"{store.Name} - {store.StoreLocation}"); }
+            foreach (Store store in storeResults) { Console.WriteLine($"{store.Name} - {store.StoreLocation}"); }
             Console.WriteLine(AllStoresOnLevel(stores, "E").Count); // 0
         }
 
-            public static bool ContainsStore(string[] storeList, string searchKey)
+
+
+        public static bool ContainsStore(string[] storeList, string searchKey)
         {
             foreach (string store in storeList)
             {
@@ -128,6 +310,9 @@ namespace GA_LinearSearch
             };
 
         } // End Preload
+
+
+
 
     }
 }
